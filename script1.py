3a4b95f9fbb2614d6ae088dd20ab73d1b189965f
GrayPearson <122818113+GrayPearson@users.noreply.github.com>
2023-05-17 15:16:48 -0400

    Moved a line from script1 to 2

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Prints the first 2 lines from Zen of Python
 print("Beautiful is better than ugly.")
-print("Explicit is better than implicit.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Prints lines 8 and 9 of Zen of Python
 print("Special cases aren't special enough to break the rules.")
 print("Although practicality beats purity.")
+print("Explicit is better than implicit.")
